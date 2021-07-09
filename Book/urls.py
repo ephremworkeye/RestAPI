a364fdbac8f67ef86bc694d49625c3dbdb86864d
ephremworkeye <ephremworkeye@gmail.com>
2021-07-08 19:26:05 -0700

    passing data of multiple tables to single template

--- Book/urls.py
+++ Book/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BookList
+from .views import BookList, Home
 from . import api_views
 
 
 urlpatterns = [
     path('', BookList.as_view(), name='book_lsit'),
+    path('home', Home.as_view(), name='home'),
     path('api', api_views.count_books),
     path('all', api_views.all_books),
 ]
--- Book/views.py
+++ Book/views.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.base import TemplateView
 from .models import Author, Book
 
 # Create your views here.
@@ -9,3 +10,16 @@ class BookList(ListView):
     model = Book
     queryset = Book.objects.all()
     template_name = 'book/home.html'
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super(Home, self).get_context_data(*args, **kwargs)
+        context['books'] = Book.objects.all()
+        context['authors'] = Author.objects.all()
+        return context
+
+       
+
+
