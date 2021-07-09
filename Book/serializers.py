baca56796f7f4a478d2b003c2638eb464b750823
ephremworkeye <ephremworkeye@gmail.com>
2021-07-09 00:43:57 -0700

    class based api and model serializer added

--- Book/serializers.py
+++ Book/serializers.py
@@ -1,15 +1,28 @@
 from django.db.models.base import Model
 from rest_framework import serializers
+from .models import Book, Author
 
-class AuthorSerilizer(serializers.Serializer):
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    email = serializers.EmailField()
-    username = serializers.CharField()
+# class AuthorSerilizer(serializers.Serializer):
+#     first_name = serializers.CharField()
+#     last_name = serializers.CharField()
+#     email = serializers.EmailField()
+#     username = serializers.CharField()
 
 
-class BookSerializer(serializers.Serializer):
+# class BookSerializer(serializers.Serializer):
+#     author = AuthorSerilizer()
+#     title = serializers.CharField()
+#     slug = serializers.SlugField()
+#     description = serializers.CharField() 
+
+# class based serializers
+class AuthorSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['first_name', 'last_name', 'email', 'username']
+
+class BookSerilizer(serializers.ModelSerializer):
     author = AuthorSerilizer()
-    title = serializers.CharField()
-    slug = serializers.SlugField()
-    description = serializers.CharField() 
+    class Meta:
+        model = Book
+        fields = ['title', 'slug', 'author']
--- Book/urls.py
+++ Book/urls.py
@@ -4,8 +4,9 @@ from . import api_views
 
 
 urlpatterns = [
-    path('', BookList.as_view(), name='book_lsit'),
-    path('home', Home.as_view(), name='home'),
-    path('api', api_views.count_books),
-    path('all', api_views.all_books),
+    # path('', BookList.as_view(), name='book_lsit'),
+    # path('home', Home.as_view(), name='home'),
+    # path('api', api_views.count_books),
+    # path('all', api_views.all_books),
+    path('api/allbooks', api_views.AllBooks.as_view(), name='all_books'), # from model serializer
 ]
